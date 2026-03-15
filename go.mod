910996dd23343e626bf76bb1a9e097e9c42dfcd1
Bospur <bospobob@yandex.ru>
2026-03-15 22:24:44 +0500

    add db connection, migrations, project structure

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module go-server
 
-go 1.22
+go 1.24.0
+
+require (
+	github.com/golang-migrate/migrate/v4 v4.19.1
+	github.com/lib/pq v1.11.2
+)
--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+
+	"go-server/internal/db"
 )
 
 type Response struct {
@@ -20,14 +23,30 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// Читаем строку подключения из переменной окружения
+	databaseURL := os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		log.Fatal("DATABASE_URL не задан")
+	}
+
+	// Подключаемся к БД
+	database, err := db.Connect(databaseURL)
+	if err != nil {
+		log.Fatalf("не удалось подключиться к БД: %v", err)
+	}
+	defer database.Close()
+	log.Println("подключение к БД установлено")
+
+	// Применяем миграции
+	if err := db.RunMigrations(database); err != nil {
+		log.Fatalf("ошибка миграций: %v", err)
+	}
 
 	http.HandleFunc("/hello", helloHandler)
 
 	log.Println("server started :8080")
 
-	err := http.ListenAndServe(":8080", nil)
-
-	if err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
