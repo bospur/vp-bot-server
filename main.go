90b2e40498ec24cc679e8866b21e57df116b9887
Bospur <bospur@MacBook-Pro-Bospur.local>
2026-03-15 09:16:33 +0500

    start

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	res := Response{
+		Message: "Hello from Go server",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
+}
+
+func main() {
+
+	http.HandleFunc("/hello", helloHandler)
+
+	log.Println("server started :8080")
+
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
